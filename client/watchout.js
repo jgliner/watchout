39cab49e5a093487c0562c72b925934145d7b0e2
jgliner <jarrett.gliner@gmail.com>
2016-01-22 17:14:45 -0800

    Initialize object for enemy positions

--- client/watchout.js
+++ client/watchout.js
@@ -1,4 +1,6 @@
 // start slingin' some d3 here.
+
+// init
 var h = +d3.select('body').select('.stage').style('height').slice(0,-2);
 var w = +d3.select('body').select('.stage').style('width').slice(0,-2);
 
@@ -12,3 +14,18 @@ var gameStats = {
   score: 0,
   lives: 0
 };
+
+// floating head enemies
+var createEnemies = [];
+for (var i = 0; i < gameOptions.nEnemies; i++) {
+  createEnemies.push(i);
+}
+
+createEnemies = createEnemies.map(function(enemy) {
+  var obj = {
+    id: enemy,
+    x: Math.random()*w,
+    y: Math.random()*h
+  };
+  return obj;
+});
