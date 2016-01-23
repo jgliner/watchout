c7949a71009bbf922295da1a1427f2760e247e6c
jgliner <jarrett.gliner@gmail.com>
2016-01-22 16:16:21 -0800

    Initialize game vars and eliminate Bootstrap
    (doesn't play nice with SVGs)

--- client/watchout.js
+++ client/watchout.js
@@ -1,3 +1,14 @@
 // start slingin' some d3 here.
+var h = +d3.select('body').select('.stage').style('height').slice(0,-2);
+var w = +d3.select('body').select('.stage').style('width').slice(0,-2);
 
-// draw enemies
+var gameOptions = {
+  height: h,
+  width: w,
+  nEnemies: 30
+};
+
+var gameStats = {
+  score: 0,
+  lives: 0
+};
