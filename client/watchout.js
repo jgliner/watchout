84bf647687c8e8dfba066130fbbd7ff7d5ccf67b
jgliner <jarrett.gliner@gmail.com>
2016-01-23 17:00:29 -0800

    Detect collisions appropriately
    TODO: increment score

--- client/watchout.js
+++ client/watchout.js
@@ -7,7 +7,7 @@ var w = +d3.select('body').select('.stage').style('width').slice(0,-2);
 var gameOptions = {
   height: h,
   width: w,
-  nEnemies: 1
+  nEnemies: 15
 };
 
 var gameStats = {
@@ -18,6 +18,7 @@ var gameStats = {
 // IIFE enemy generator runs at load
 
 function init() {
+  var mouse = [0,0];
   // floating head enemies
   var createEnemies = [];
   for (var i = 0; i < gameOptions.nEnemies; i++) {
@@ -32,7 +33,7 @@ function init() {
     return obj;
   });
 
-  //make every enemny in the array
+  //make every enemy in the array
 
   var heads = d3.select('body').select('svg').selectAll('.enemy')
     .data(createEnemies, function(d) {
@@ -41,8 +42,8 @@ function init() {
   
   var svg = d3.select("svg");
   svg.on("mousemove",function() {
-    var coords = d3.mouse(this);
-    console.log(coords);
+    mouse = d3.mouse(this);
+    
   });
 
 
@@ -62,26 +63,43 @@ function init() {
     })
     .attr('class', 'enemy');
 
-    setInterval(function() {
-      heads.transition()
-      .duration(1500)
-      .tween("text", function() {
-        var i = d3.interpolateRound(this.x.baseVal.value, this.x.animVal.value);
-        var j = d3.interpolateRound(this.y.baseVal.value, this.y.animVal.value);
-        // var d = d3.interpolateRound(this.x.animVal.value,this.y.animVal.value);
-
-        return function(t) {
-          // this.textContext = i(t)
-          // console.log(i(t) + " , " + j(t));
-        }
+  var move = function(element) {
+    element.transition().duration(5000).ease('bounce')
+      .attr('x', function(d) {
+        return Math.random()*(w-100);
       })
-    .attr('x', function(d) {
-      return Math.random()*(w-100);
-    })
-    .attr('y', function(d) {
-      return Math.random()*(h-100);
+      .attr('y', function(d) {
+        return Math.random()*(h-100);
+      })
+      .each('end', function() {
+        move(d3.select(this));
+      });
+  };
+  move(heads);
+
+  var prevCollisionState = false;
+  var collisions = function() {
+    var collision = false;
+    heads.each(function() {
+      var offsetX = this.x.animVal.value + 50;
+      var offsetY = this.y.animVal.value + 50;
+
+      var mouseX = offsetX - (mouse[0]);
+      var mouseY = offsetY - (mouse[1]);
+
+      if (Math.sqrt(mouseX*mouseX + mouseY*mouseY) < 32) {
+        collision = true;
+      }
     });
-  }, 1500);
+    if (collision) {
+      if (prevCollisionState !== collision) {
+        gameStats.lives--;
+      }
+    }
+    prevCollisionState = collision;
+
+  };
+  d3.timer(collisions);
 }
 init();
 
